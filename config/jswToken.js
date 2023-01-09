8dfcb0ba761617e2e1ac738c4dc47d2ca5e32b14
quan <nguyenquan07112001@gmail.com>
2023-01-09 09:50:40 +0700

    Get User

---
+++ config/jswToken.js
@@ -0,0 +1,6 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SERCRET, { expiresIn: "3d" });
+};
+module.exports = { generateToken };
--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jswToken");
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
@@ -17,10 +18,42 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   //check if user exists or not
   const findUser = await User.findOne({ email });
   if (findUser && (await findUser.isPasswordMatched(password))) {
-    res.json(findUser);
+    res.json({
+      _id: findUser?.id,
+      firstName: findUser?.firstName,
+      lastName: findUser?.lastName,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
   } else {
     throw new Error("Invalid Credential");
   }
 });
 
-module.exports = { createUser, loginUserCtrl };
+//Get all user
+
+const getAllUser = asyncHandler(async (req, res) => {
+  try {
+    const getUSers = await User.find();
+    res.json(getUSers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Get a single user
+
+const getAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const getaUser = await User.findById(id);
+    res.json({
+      getaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createUser, loginUserCtrl, getAllUser, getAUser };
