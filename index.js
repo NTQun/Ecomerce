9a572bc70b27d9d37db0737872f1aeebd662b53a
quan <nguyenquan07112001@gmail.com>
2023-01-10 16:04:34 +0700

    Productmodle , handdler create+geta+getall

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
-const { default: mongoose } = require("mongoose");
-const bodyParser = require("body-parser");
-const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const productRouter = require("./routes/productRoute");
 const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
 
 mongoose.set("strictQuery", true);
-
 dbConnect();
 
 app.use(bodyParser.json());
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
 
 app.use(notFound);
 app.use(errorHandler);
