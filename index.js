d3ca67a4122844d7fc7940f4d97d909f7c4558b4
quan <nguyenquan07112001@gmail.com>
2023-01-24 16:20:42 +0700

    Blogcategory

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/procategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
+
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const { default: mongoose } = require("mongoose");
@@ -24,6 +26,7 @@ app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
