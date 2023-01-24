bf5aeb013d0bac30cc6449cd7a5967f77b2139f0
quan <nguyenquan07112001@gmail.com>
2023-01-24 15:48:55 +0700

    create, update  prodcategory

---
+++ controller/procategoryCtrl.js
@@ -0,0 +1,25 @@
+const Category = require("../models/procategoryModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = { createCategory, updateCategory };
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
-
+const categoryRouter = require("./routes/procategoryRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const { default: mongoose } = require("mongoose");
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
