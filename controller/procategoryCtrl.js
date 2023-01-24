9285a9147013d4b5258804c0355b9a1d3d698a95
quan <nguyenquan07112001@gmail.com>
2023-01-24 15:57:40 +0700

    delete, get category

--- controller/procategoryCtrl.js
+++ controller/procategoryCtrl.js
@@ -13,6 +13,7 @@ const createCategory = asyncHandler(async (req, res) => {
 
 const updateCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  validateMongoDbId(id);
   try {
     const updateCategory = await Category.findByIdAndUpdate(id, req.body, {
       new: true,
@@ -22,4 +23,32 @@ const updateCategory = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
-module.exports = { createCategory, updateCategory };
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deleteCategory = await Category.findByIdAndDelete(id);
+    res.json(deleteCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getCategory = await Category.findById(id);
+    res.json(getCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+};
